0099efcaeae40c4d54d80f793ee5ed66749326f7
PATILSHUBHAM69 <patilshubhamy@outlook.com>
2023-07-06 12:51:42 +0530

    first commit

---
+++ database/DB_Connection.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func Connect() {
+	var err error
+
+	// Make Database Connection
+	db, err = sql.Open("mysql", "root:india@123@tcp(localhost:3306)/hc_patient_data")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MySQL database!")
+
+	//Create Tabel
+	Create_Patient, err := db.Query("CREATE TABLE IF NOT EXISTS PatientDetails (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(80), age INT, gender VARCHAR(10), contact_no VARCHAR(255), medical_history TEXT );")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer Create_Patient.Close()
+	fmt.Println("Patient Details Table Successfully Created")
+}
