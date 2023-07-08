ace780827a377c2776904d8b2531eaa37a3f36a5
PATILSHUBHAM69 <patilshubhamy@outlook.com>
2023-07-08 18:17:34 +0530

    refine code 2

--- controllers/patientFunc.go
+++ controllers/patientFunc.go
@@ -27,49 +27,40 @@ func CreatePatient(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
 	stmt, err := db.Prepare("INSERT INTO PatientDetails (name, age, gender, contact, medical_history) VALUES (?, ?, ?, ?, ?)")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
-
 	result, err := stmt.Exec(patient.Name, patient.Age, patient.Gender, patient.Contact, patient.MedicalHistory)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	patientID, err := result.LastInsertId()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "Patient created with ID: %d", patientID)
 }
 
 func GetPatient(w http.ResponseWriter, r *http.Request) {
-	var requestBody struct {
-		PatientID int `json:"patient_id"`
-	}
-
-	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	var patient models.Patient
+	err := json.NewDecoder(r.Body).Decode(&patient)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	patientID := requestBody.PatientID
-
+	patientID := patient.ID
 	rows, err := db.Query("SELECT id, name, age, gender, contact, medical_history FROM PatientDetails WHERE id = ?", patientID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer rows.Close()
-
 	if rows.Next() {
 		var patient models.Patient
 		err := rows.Scan(&patient.ID, &patient.Name, &patient.Age, &patient.Gender, &patient.Contact, &patient.MedicalHistory)
@@ -77,8 +68,6 @@ func GetPatient(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		w.Header().Set("Content-Type", "application/json")
 		jsonData, err := json.Marshal(patient)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -91,79 +80,45 @@ func GetPatient(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdatePatient(w http.ResponseWriter, r *http.Request) {
-
-	var requestBody struct {
-		PatientID      int    `json:"patient_id"`
-		Name           string `json:"name"`
-		Age            int    `json:"age"`
-		Gender         string `json:"gender"`
-		Contact        string `json:"contact"`
-		MedicalHistory string `json:"medical_history"`
-	}
-
-	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	var patient models.Patient
+	err := json.NewDecoder(r.Body).Decode(&patient)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	patientID := requestBody.PatientID
-
-	patient := models.Patient{
-		ID:             patientID,
-		Name:           requestBody.Name,
-		Age:            requestBody.Age,
-		Gender:         requestBody.Gender,
-		Contact:        requestBody.Contact,
-		MedicalHistory: requestBody.MedicalHistory,
-	}
-
 	stmt, err := db.Prepare("UPDATE PatientDetails SET name=?, age=?, gender=?, contact=?, medical_history=? WHERE id=?")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
-
 	_, err = stmt.Exec(patient.Name, patient.Age, patient.Gender, patient.Contact, patient.MedicalHistory, patient.ID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Patient updated successfully")
 }
 
 func DeletePatient(w http.ResponseWriter, r *http.Request) {
-	var requestBody struct {
-		PatientID int `json:"patient_id"`
-	}
-
-	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	var patient models.Patient
+	err := json.NewDecoder(r.Body).Decode(&patient)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	patientID := requestBody.PatientID
-	patient := models.Patient{
-		ID: patientID,
-	}
-
 	stmt, err := db.Prepare("DELETE FROM PatientDetails WHERE id=?")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
-
 	_, err = stmt.Exec(patient.ID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Patient deleted successfully")
 }
