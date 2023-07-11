a011dd11f43f9942615b8a649a9c65ea8435b3fa
PATILSHUBHAM69 <107848127+PATILSHUBHAM69@users.noreply.github.com>
2023-07-11 09:58:31 +0530

    remove html

--- main.go
+++ main.go
@@ -1,30 +1,3 @@
-// package main
-
-// import (
-// 	"log"
-// 	"net/http"
-
-// 	"github.com/PATILSHUBHAM69/HC_Patient_Data/controllers"
-// 	"github.com/PATILSHUBHAM69/HC_Patient_Data/database"
-// )
-
-// func main() {
-// 	database.Connect()
-
-// 	// Serve static files (HTML, CSS, JS)
-// 	fs := http.FileServer(http.Dir("static"))
-// 	http.Handle("/", fs)
-
-// 	// Register API endpoints
-// 	http.HandleFunc("/create_patient", controllers.CreatePatient)
-// 	http.HandleFunc("/get_patient", controllers.GetPatient)
-// 	http.HandleFunc("/update_patient", controllers.UpdatePatient)
-// 	http.HandleFunc("/delete_patient", controllers.DeletePatient)
-
-// 	// Start the server
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
-
 package main
 
 import (
